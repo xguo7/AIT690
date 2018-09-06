7a6e6bbfe07f2bbd4287f1d469b5a4e40b7251b0
william ermlcik <ermlickw@vt.edu>
2018-09-05 21:20:12 -0400

    first

---
+++ Assignment1/Assignment1.py
@@ -0,0 +1,8 @@
+"""
+AIT 690 | Assignment 1 | Due 9/19/2018
+Billy Ermlick
+Nidhi
+X
+
+Description here...
+"""
---
+++ tutorial.py
@@ -0,0 +1,8 @@
+# 9/5/2018
+# python intro
+import nltk
+import re
+print('hello')
+import sys
+sys.stdin
+sys.stdout.write("stuff")
