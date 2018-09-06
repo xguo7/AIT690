16b667b4d5c0a5074fc5cb9ce67188e7a72c5b2a
william ermlcik <ermlickw@vt.edu>
2018-09-05 21:36:21 -0400

    'second'

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -2,7 +2,16 @@
 AIT 690 | Assignment 1 | Due 9/19/2018
 Billy Ermlick
 Nidhi
-X
+Xiaojie Guo
 
 Description here...
+check out default dict...
+
+
 """
+from collections import defaultdict
+
+pet = defaultdict(lambda: 'dog')
+pet['kai'] = 'snake'
+
+print(pet['kevin'])
